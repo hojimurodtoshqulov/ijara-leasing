f351666a60a8a268ad73a7acc2869219b06d031a
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-04-08 00:54:10 +0500

    updated

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,16 +1,35 @@
 import scss from "./footer.module.scss";
 import logo from "../../media/logo_head.png";
+import img1 from "../../media/Frame163.png";
+import img2 from "../../media/Frame164.png";
+import img3 from "../../media/Frame165.png";
 import { Link } from "react-router-dom";
+import { FaFacebookF, FaTwitter } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
 const Footer = () => {
 	return (
 		<footer className={scss.footer}>
 			<div className="container">
 				<div className={scss.footer__row1}>
 					<div className={scss.footer__row1_col1}>
-						<Link to="/">
+						<Link to="/" className={scss.footer__row1_col1_logo}>
 							<img src={logo} alt="" />
 							<h2>ijara leasing</h2>
 						</Link>
+						<div className={scss.footer__row1_col1_imgDiv}>
+							<Link to="/" className={scss.footer__row1_col1_imgDiv_img}>
+								{/* <img src={img1} alt="" /> */}
+								<FaFacebookF />
+							</Link>
+							<Link to="/" className={scss.footer__row1_col1_imgDiv_img}>
+								{/* <img src={img2} alt="" /> */}
+								<FaTwitter />
+							</Link>
+							<Link to="/" className={scss.footer__row1_col1_imgDiv_img}>
+								{/* <img src={img3} alt="" /> */}
+								<RiInstagramFill />
+							</Link>
+						</div>
 					</div>
 					<div className={scss.footer__row1_line}></div>
 					<div className={scss.footer__row1_col2}>
@@ -32,11 +51,12 @@ const Footer = () => {
 					</div>
 					<div className={scss.footer__row1_line}></div>
 					<div className={scss.footer__row1_col3}>
-						<h1>Visit</h1>
-						<p>Logan Barker, 865 Oak Boulevard, Elk Grove, Vermont 95184</p>
-						<h1>Call</h1>
+						<h1>Наш адрес:</h1>
+						<p>Юнусабадский район, Ц-5, д.78, кв.13</p>
+						<h1>Телефон:</h1>
 						<p>
-							<Link to="tel:01761531774">017-6153-1774</Link>
+							<a href="tel:+998991609000">+998 (99) 160-90-00</a>
+							<a href="tel:+998991619000">+998 (99) 161-90-00</a>
 						</p>
 					</div>
 				</div>
