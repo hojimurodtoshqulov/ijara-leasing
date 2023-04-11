75d34a404316d963a29980d18928ba67b540ebc9
diyorbek-yusupov <dyuzb2004@gmail.com>
2023-04-11 12:43:15 +0500

    map fixed

--- src/components/location/location.js
+++ src/components/location/location.js
@@ -3,47 +3,9 @@ import "./location.scss";
 import {useTranslation} from "react-i18next";
 import {motion} from "framer-motion";
 
-import {MapContainer} from 'react-leaflet/MapContainer'
-import {TileLayer} from 'react-leaflet/TileLayer'
-import {useMap} from 'react-leaflet/hooks'
-import {Marker, Popup} from "react-leaflet";
-
-// import L from "leaflet";
-
 const Location = () => {
     const {t} = useTranslation();
     const transition = {type: "spring", duration: 2};
-
-    useEffect(() => {
-        // const myMap = L.map("mapid").setView([50.7940939, 41.1352718], 3);
-        //
-        // L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        //     attribution:
-        //         'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-        //     maxZoom: 18,
-        // }).addTo(myMap);
-
-        // L.marker([41.3240686, 69.2532404])
-        //     .addTo(myMap)
-        //     .bindPopup("Tashkent")
-        //     .on("click", (e) => {
-        //         e.target.openPopup();
-        //         myMap.flyTo(e.target.getLatLng(), 18, {duration: 1});
-        //         myMap.on("popupopen", () => {
-        //         });
-        //     });
-        //
-        // L.marker([59.9668311, 30.3128765])
-        //     .addTo(myMap)
-        //     .bindPopup('Школа сомелье "WineJet"')
-        //     .on("click", (e) => {
-        //         // e.target.openPopup();
-        //         myMap.flyTo(e.target.getLatLng(), 18, {duration: 1});
-        //         // myMap.on("popupopen", () => {
-        //
-        //         // });
-        //     });
-    }, [])
 
     return (
         <div className="map">
